ede29401d6195bb5eff4f84072298114370a6567
gaojiuli <gaojiuli@gmail.com>
2017-06-06 17:25:59 +0800

    Add logger to gain.__init__

--- gain/__init__.py
+++ gain/__init__.py
@@ -1,6 +1,7 @@
 from .item import Item
+from .log import logger
 from .parser import Parser
 from .selector import Css, Regex, Xpath
 from .spider import Spider
 
-__all__ = ('Css', 'Xpath', 'Item', 'Spider', 'Parser', 'Regex')
+__all__ = ('Css', 'Xpath', 'Item', 'Spider', 'Parser', 'Regex', 'logger')
