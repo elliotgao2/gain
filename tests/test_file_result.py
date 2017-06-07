d3bf41f4ad629bfe089aec4df4914884d93d474d
c1ay <c1ay@c1ay.cc>
2017-06-07 17:00:37 +0800

    test

--- tests/test_file_result.py
+++ tests/test_file_result.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append("..")
 from gain.result import FileResult
 import asyncio
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('..')
 from gain import Css, Item, Parser, Xpath
 
 
