d170f5f366a3d2f7dc2f8760c1477f6a1968916d
babykick <babykick@163.com>
2017-09-18 17:53:30 +0800

    update pagination xpath

--- example/use_xpath_parser.py
+++ example/use_xpath_parser.py
@@ -12,9 +12,11 @@ class MySpider(Spider):
     start_url = 'https://mydramatime.com/europe-and-us-drama/'
     concurrency = 5
     headers = {'User-Agent': 'Google Spider'}
-    parsers = [Parser('https://mydramatime.com/europe-and-us-drama/game-of-thrones/page/\d+?/'),
+    parsers = [
                XPathParser('//span[@class="category-name"]/a/@href'),
-               XPathParser('//div[@class="mini-left"]//div[contains(@class, "mini-title")]/a/@href', Post)]
+               XPathParser('//div[contains(@class, "pagination")]/ul/li/a[contains(@href, "page")]/@href'),
+               XPathParser('//div[@class="mini-left"]//div[contains(@class, "mini-title")]/a/@href', Post)
+              ]
 
 
 if __name__ == '__main__':
