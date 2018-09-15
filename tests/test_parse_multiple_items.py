31b19438adb318d7e21ce912fbc9ee6011aa4b40
Kelvin Wuite <kelvin@ludaro.nl>
2018-09-15 20:50:55 +0200

    Fixed Einstein Test

--- tests/test_parse_multiple_items.py
+++ tests/test_parse_multiple_items.py
@@ -1,21 +1,18 @@
 from gain import Item, Parser, Spider, Xpath
 
-
 class Post(Item):
     quote = Xpath("//span[@class = 'text']")
     author = Xpath("//small")
 
     async def save(self):
         quote = dict(zip(self.author, self.quote))
-        assert quote['Albert Einstein'] == '“Try not to become a man of success. Rather become a man of value.”'
-
+        assert quote['Albert Einstein'] == "“If you can't explain it to a six year old, you don't understand it yourself.”"
 
 class MySpider(Spider):
     concurrency = 5
     headers = {'User-Agent': 'Google Spider'}
     start_url = 'http://quotes.toscrape.com/'
-    parsers = [Parser('/page/1/'),
-               Parser('/page/1/', Post)]
+    parsers = [Parser('/page/2/'),
+               Parser('/page/2/', Post)]
 
 MySpider.run()
-
